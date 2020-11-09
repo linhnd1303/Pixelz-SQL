89aa16c10213880ec7d28f84dd01c3484b7b4c99
linhnd <linhnd1303@gmail.com>
2020-11-09 15:06:42 +0700

    date

--- Template&Customer Performance/Ssense customer performance with rejection.sql
+++ Template&Customer Performance/Ssense customer performance with rejection.sql
@@ -1,6 +1,6 @@
 
 SELECT --TOP 10000
-date(iss.AssignDate),
+dateadd(hour,7,iss.AssignDate) as date_,
 iss.AssignDate,
     SawSkill.SawSkillName,
     ProductionWorkers.WorkerName,
@@ -20,10 +20,9 @@ iss.AssignDate,
     round(1.0*sum(iss.WorkingServicePriceInMiliseconds*0.001)/count(iss.ImageID),1) as avg_opt,
     ROUND(1.0*sum(iss.WorkingTimeInMilliseconds)/nullif(sum(iss.WorkingServicePriceInMiliseconds),0),2) AS efficiency_score,
     round(sum(iss.WorkingTimeInMilliseconds*0.001),1) AS sum_ipt_hours,
-	round(sum(iss.WorkingServicePriceInMiliseconds*0.001),1) AS sum_opt_hours
+	round(sum(iss.WorkingServicePriceInMiliseconds*0.001),1) AS sum_opt_hours,
 
-
-
+  count(srl.ImageID) AS rej_count --added
 
 FROM ImageSawStep iss
 INNER JOIN ProductionWorkers ON  ProductionWorkers.WorkerID = iss.ProductionWorkerID
@@ -31,18 +30,22 @@ INNER JOIN SawSkill ON SawSkill.SawSkillID = iss.SawSkillID
 INNER JOIN Images ON Images.ImageID = iss.ImageID
 INNER JOIN Templates t ON t.TemplateName=iss."Template Name" AND iss.ContactID=t.ContactID AND t.TemplateDeleted <> 1
 INNER JOIN Contacts ON Contacts.ContactID=iss.ContactID
+INNER JOIN SawRejectionLog srl ON srl.SawSkillID = iss.SawSkillID  and srl.ImageID = iss.ImageID
 
-WHERE dateadd(hour,7,iss.AssignDate) > date_trunc('Week', getdate()) - INTERVAL '24 Weeks'
-AND 
-t.ContactID=1160192 -- Ssense Customer ID
--- t.TemplateName like 'Test template (SSENSE)%'  -- Ssense Test Template
+WHERE
+dateadd(hour,7,iss.AssignDate) > date_trunc('Week', getdate()) - INTERVAL '24 Weeks'
+AND t.ContactID=1160192 -- Ssense Customer ID
+--AND  t.TemplateName like 'Test template (SSENSE)%'  -- Ssense Test Template
 --AND ImageSawStep.ImageID > 33100000 -- greater than ~June 2019
 --AND ImageSawStep.SawSkillID = 10 -- Footwear
 AND SawSkill.SawSkillName NOT LIKE 'QA G%'
 AND ProductionWorkers.WorkerName NOT LIKE 'auto%'
+AND ReceiverWorkerID IS NOT NULL -- remove bypass rejections
+AND IsCustomerRejected IS NULL
+
 
 GROUP BY
-date(iss.AssignDate),
+date_,
 iss.AssignDate,
 SawSkill.SawSkillName,
 ProductionWorkers.WorkerName,
@@ -58,5 +61,5 @@ iss.SawSkillID,
 GlobalSortOrder
 
 
-ORDER BY date(iss.AssignDate) desc
+ORDER BY date_ desc
 LIMIT 10000 OFFSET 0 
